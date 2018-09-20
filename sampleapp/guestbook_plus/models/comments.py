3a3ab8b114bfa56251abb78175668f76810d198b
Fufuhu <e238058r@hotmail.co.jp>
2018-09-20 18:17:53 +0900

    Commentのテストを追加

--- sampleapp/guestbook_plus/models/comments.py
+++ sampleapp/guestbook_plus/models/comments.py
@@ -10,3 +10,29 @@ class Comment(models.Model):
     commentator = models.ForeignKey(to=Commentator, on_delete=models.CASCADE, blank=True)
     parent = models.ForeignKey('Comment', on_delete=models.CASCADE, related_name='oyako', null=True)
     append_at = models.DateTimeField(auto_now_add=True)
+
+    def get_thread(self):
+        """
+        特定のcommentからcommentの所属するスレッド全体を取得する
+        """
+
+        comment = self
+        while comment.parent is not None:
+            comment = comment.parent
+        
+        self.tree = [comment]
+        self.__get_leafs(root=comment)
+
+        return self.tree
+        
+    
+    def __get_leafs(self, root):
+        leafs = Comment.objects.filter(parent=root)
+
+        self.tree = self.tree + list(leafs)
+
+        if leafs.count() == 0:
+            return self.tree
+        else:
+            for leaf in leafs:
+                self.__get_leafs(root=leaf)

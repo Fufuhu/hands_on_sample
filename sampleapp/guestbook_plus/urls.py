41d5898d2dbe847600a69045fb4d3ca34ab1b03a
Fufuhu <e238058r@hotmail.co.jp>
2018-09-25 21:47:44 +0900

    /guestbook/へのルーティングを修正

--- sampleapp/guestbook_plus/urls.py
+++ sampleapp/guestbook_plus/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
-from guestbook_plus.views.base_view import BaseView
 from .views.comment_list_view import CommentListView
 from .views.comment_thread_view import CommentThreadView
 
 app_name = 'guestbook_plus'
 
 urlpatterns = [
-    url(r'^$', BaseView.as_view(), name='base'),
+    url(r'^$', CommentListView.as_view(), name='base'),
     url(r'^comments/$', CommentListView.as_view(), name='comment_list'),
     url(r'^comments/(.+)/$', CommentThreadView.as_view(), name='comment_thread'),
 ]
--- sampleapp/guestbook_plus/views/base_view.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import View
-from django.shortcuts import render
-
-class BaseView(View):
-    def get(self, request):
-        response_params = {}
-        return render(request, 'base.html', response_params)

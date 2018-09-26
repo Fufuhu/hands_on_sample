654106b0437f0213c983aa79c2b493e87ff71653
Fufuhu <e238058r@hotmail.co.jp>
2018-09-26 12:29:06 +0900

    docker-compose対応を実施

--- sampleapp/sampleapp/settings_prod.py
+++ sampleapp/sampleapp/settings_prod.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'sampleapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'NAME': 'guestbook',
         'USER': 'guestbook',
